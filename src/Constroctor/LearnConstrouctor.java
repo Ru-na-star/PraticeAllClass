823d1dd908f3ec07cbdad70cefc5a9a0ff9de6af
Ru-na-star <runa11427@gmail.com>
2020-10-05 20:49:48 -0400

    first commit

---
+++ src/Constroctor/LearnConstrouctor.java
@@ -0,0 +1,16 @@
+package Constroctor;
+
+public class LearnConstrouctor {
+// constructor name sh0uld be same as a class name
+    //constructor is a initlaize object
+    //default constructor
+    // parameterize constroctor
+    public LearnConstrouctor(){
+        System.out.println(" hello");
+    }
+public LearnConstrouctor( String namew){
+   LearnConstrouctor abby=new LearnConstrouctor("fyhrjgjhg")   ;
+}
+
+
+}
